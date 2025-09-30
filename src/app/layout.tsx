8982eda55d67719c1310f231c46f3ff1721e474c
Hari Suong <suongmec39@gmail.com>
2025-09-30 11:49:02 +0700

    update blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,8 +71,8 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </Suspense>
-        <GoogleAnalytics gaId='G-ES23SL8JK8' />
       </body>
+      <GoogleAnalytics gaId='G-E5Z3SLRJX8' />
     </html>
   )
 }
