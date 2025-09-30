e7fe14f341acbddb3f544a570cc6a0e5c30cfaf1
Hari Suong <suongmec39@gmail.com>
2025-09-30 11:00:55 +0700

    update blog

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://www.getsynapse.tech/',
-  generateRobotsTxt: true, // tự động tạo robots.txt
-  sitemapSize: 5000, // nếu >5000 URL thì nó sẽ chia file
-  changefreq: 'daily',
-  priority: 0.7,
-}
